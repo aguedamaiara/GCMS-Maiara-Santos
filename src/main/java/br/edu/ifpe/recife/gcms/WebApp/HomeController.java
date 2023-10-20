dde55569e40764b3716b35427cde0f94315006fe
aguedamaiara <costaaguedama@gmail.com>
2023-10-20 19:47:32 -0300

    1o commit

---
+++ src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
@@ -0,0 +1,18 @@
+package br.edu.ifpe.recife.gcms.WebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public @ResponseBody String greeting() {
+		return "Hello, World";
+	}
+
+	@RequestMapping("/maiara")
+	public @ResponseBody String greetingMaiara() {
+		return "Hello, Maiara!";
+	}
+}
